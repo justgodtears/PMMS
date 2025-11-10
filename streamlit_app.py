ddc1414ff220144d5dc2007be872b37434b6007a
oli-dev-ai <oopyrchal@pm.me>
2025-11-10 20:47:16 +0100

    zmiana opisu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Route Calculator")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "App for calculating cost of transport"
 )
