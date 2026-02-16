cd8d520f9aa71370fd7d20f379faf03f98987688
oli-dev-ai <oopyrchal@pm.me>
2026-02-16 11:19:32 +0100

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,6 @@ st.logo("logo2.png",size="large", icon_image="logo2.png")
 
 #definiowanie stron
 home = st.Page("home.py", title="Home", icon=":material/home:")
-dashboard = st.Page("dashboard.py", title="Dashboard", icon=":material/dashboard:")
-route_calculator = st.Page("routcalc.py", title="Route Calculator", icon=":material/calculate:")
-route_status = st.Page("route_status.py", title="Route Status", icon=":material/garage_check:")
 converter = st.Page("converter.py", title="Converter", icon=":material/table_convert:")
 
 
